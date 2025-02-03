ec99930d09d5d700ac9fa9269d940ad00a23efcd
youngishcord <kulikobyura@mail.ru>
2025-02-03 18:16:36 +0300

    начал делать эвенты. Можно добавлять текст простейшим образом

--- include/plaintext2.h
+++ include/plaintext2.h
@@ -8,11 +8,14 @@
 Так будет проще всего растягивать виджет vscollpanel
 
 
-Текст всегда рисуется на центре своей координаты с поправкой на выравнивание текста
-как на картинках 
+Текст всегда рисуется на центре своей координаты с поправкой на 
+выравнивание текста как на картинках 
 docs\images\aling_center_in_a_corner.png
 docs\images\aling_left_in_a_corner.png
 docs\images\aling_right_in_a_corner.png
+Получается необходимо настраивать оффсет для каждого выравнивания в 
+индивидуальном порядке
+
 */
 
 #ifndef PLAINTEXT2
@@ -30,6 +33,50 @@ using namespace nanogui;
 // Дока
 // https://github.com/memononen/nanovg/blob/master/src/nanovg.h#L625
 
+// Тут будет вся работа с текстом и его отрисовкой 
+class TextAreaWidget : public Widget {
+public:
+    TextAreaWidget(Widget *parent) : Widget(parent) {}
+
+    void draw(NVGcontext *ctx) override {
+        Widget::draw(ctx);
+
+        nvgFontSize(ctx, fontSize());
+        nvgFontFace(ctx, "sans");
+        nvgTextAlign(ctx, NVG_ALIGN_LEFT | NVG_ALIGN_MIDDLE);
+        // nvgTextAlign(ctx, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
+        // nvgTextAlign(ctx, NVG_ALIGN_RIGHT | NVG_ALIGN_MIDDLE);
+        NVGtextRow rows[1024];
+        int lines = nvgTextBreakLines(ctx, text.c_str(), nullptr, 300.0f-10, rows, 1024);
+
+        for (int i = 0; i < lines; i++) {
+            nvgText(ctx, mPos.x(), mPos.y()+10+20*i, rows[i].start, rows[i].end);
+            drawTestRect(ctx, mPos.x(), mPos.y()+20*i, 300-10, 20);
+        }
+    }
+
+    void drawTestRect(NVGcontext *ctx, float x, float y, float width, float heigh,
+                    Color color = Color(255, 0, 0, 255), int strokeWidth = 2)
+    {
+        nvgBeginPath(ctx);
+        nvgRect(ctx, x, y, width, heigh);
+        nvgStrokeColor(ctx, color);
+        nvgStrokeWidth(ctx, strokeWidth);
+        nvgStroke(ctx);
+        nvgClosePath(ctx);
+    }   
+
+    virtual bool keyboardEvent(int key, int scancode, int action, int modifiers) override;
+    virtual bool mouseButtonEvent(const Vector2i &p, int button, bool down, int modifiers) override;
+    virtual bool keyboardCharacterEvent(unsigned int codepoint) override;
+
+public:
+    std::string text = "Of course we can still daisy chain cubic Bézier "
+        "curves by adding another curve to our path. Lets add the second pair "
+        "of quadratic curves as a single cubic curve to our current path:";
+    // int offset = 10;
+};
+
 class PlainText2 : public Widget {
 public:
     enum class Alignment {
@@ -39,8 +86,9 @@ public:
     };
 
     struct Cursor {
-        int row, index, textIndex;
-        float posX, posY;
+        int row, index, textIndex; // Строка и индекс в многострочном тексте, 
+                                   // полнотекстовый индекс для редактирования
+        float posX, posY;          // Положение для рисования курсора
     };
 
     struct TextBlock
@@ -62,56 +110,19 @@ public:
 
 	virtual void draw(NVGcontext *ctx) override;
 
+    bool keyboardEvent(int key, int scancode, int action, int modifiers) override;
+    virtual bool keyboardCharacterEvent(unsigned int codepoint) override;
+
 protected:
     std::string text;
     Cursor cursor;
 
     // scroll panel
     VScrollPanel *vscroll;
+    TextAreaWidget* textArea;
 };
 
 
-class TextAreaWidget : public Widget {
-public:
-    TextAreaWidget(Widget *parent) : Widget(parent) {}
 
-    void draw(NVGcontext *ctx) override {
-        Widget::draw(ctx);
-
-        std::string text = "Of course we can still daisy chain cubic Bézier "
-        "curves by adding another curve to our path. Lets add the second pair "
-        "of quadratic curves as a single cubic curve to our current path:";
-
-        nvgFontSize(ctx, fontSize());
-        nvgFontFace(ctx, "sans");
-        nvgTextAlign(ctx, NVG_ALIGN_LEFT | NVG_ALIGN_MIDDLE);
-        // nvgTextAlign(ctx, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
-        // nvgTextAlign(ctx, NVG_ALIGN_RIGHT | NVG_ALIGN_MIDDLE);
-        // this->absolutePosition()
-        // std::cout << absolutePosition() << ", " << mPos.y() << "\n";
-        NVGtextRow rows[1024];
-        int lines = nvgTextBreakLines(ctx, text.c_str(), nullptr, 300.0f-10, rows, 1024);
-
-        for (int i = 0; i < lines; i++) {
-            nvgText(ctx, mPos.x()+10, mPos.y()+10+20*i, rows[i].start, rows[i].end);
-            drawTestRect(ctx, mPos.x()+10, mPos.y()+20*i, 300-10, 20);
-            
-        }
-    }
-
-    void drawTestRect(NVGcontext *ctx, float x, float y, float width, float heigh,
-                    Color color = Color(255, 0, 0, 255), int strokeWidth = 2)
-    {
-        nvgBeginPath(ctx);
-        nvgRect(ctx, x, y, width, heigh);
-        nvgStrokeColor(ctx, color);
-        nvgStrokeWidth(ctx, strokeWidth);
-        nvgStroke(ctx);
-        nvgClosePath(ctx);
-    }   
-
-protected:
-    // int offset = 10;
-};
 
 #endif // PLAINTEXT2
--- src/plaintext2.cpp
+++ src/plaintext2.cpp
@@ -12,47 +12,14 @@ PlainText2::PlainText2(Widget *parent, const std::string &text)
 	, text(text) {
 	vscroll = new VScrollPanel(this);
 	vscroll->setFixedSize(nanogui::Vector2i(300, 300));
-
-	Widget *wrapper = new Widget(vscroll);
-	wrapper->setLayout(new BoxLayout(Orientation::Vertical));
-
-	// wrapper->setFixedSize(nanogui::Vector2i(300 ,300));
 	
-	TextAreaWidget* textArea = new TextAreaWidget(wrapper);
+	textArea = new TextAreaWidget(vscroll);
 	textArea->setFixedWidth(300); // Обязательно надо указывать ширину, иначе она нулевая
 	textArea->setFixedHeight(1000);
 
 	// Button *bt;
 	// bt = new Button(wrapper, "te");
 	// bt->setFixedHeight(200);
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-	// bt = new Button(wrapper, "te");
-
 }
 
 void PlainText2::draw(NVGcontext *ctx) {
@@ -83,3 +50,157 @@ void PlainText2::draw(NVGcontext *ctx) {
 
 
 }
+
+bool PlainText2::keyboardEvent(int key, int scancode, int action, int modifiers) {
+	std::cout << "plaitext keyboardEvent\n";
+	// Widget::keyboardEvent(key, scancode, action, modifiers);
+	return false;
+}
+
+bool PlainText2::keyboardCharacterEvent(unsigned int codepoint) {
+	std::cout << "plaitext keyboardCharacterEvent\n";
+	// Widget::keyboardCharacterEvent(codepoint);
+	std::ostringstream convert;
+	convert << (char) codepoint;
+	textArea->text.append(convert.str());
+	return false;
+}
+
+
+bool TextAreaWidget::mouseButtonEvent(const Vector2i &p, int button, bool down, int modifiers) {
+	// std::cout << "click!\n";
+	if (button == GLFW_MOUSE_BUTTON_1 && down) {
+		std::cout << p.x() << ", " << p.y() << "\n";
+	}
+	return false;
+}
+
+bool TextAreaWidget::keyboardCharacterEvent(unsigned int codepoint) {
+	std::cout << "caracter event\n";
+	return false;
+}
+
+bool TextAreaWidget::keyboardEvent(int key, int scancode, int action, int modifiers) {
+	// Widget::keyboardEvent(key, scancode, action, modifiers);
+	std::cout << "keyboard event\n";
+	// text.append()
+	return false;
+}
+    // if (mEditable && focused()) {
+    //     if (action == GLFW_PRESS || action == GLFW_REPEAT) {
+    //         if (key == GLFW_KEY_LEFT) {
+    //             if (modifiers == GLFW_MOD_SHIFT) {
+    //                 if (mSelectionPos == -1)
+    //                     mSelectionPos = mCursorPos;
+    //             } else {
+    //                 mSelectionPos = -1;
+    //             }
+
+    //             if (mCursorPos > 0) {
+    //                 mCursorPos--;
+    //                 cursor.textIndex--;
+    //             } else if (mCursorRow > 0) {
+    //                 mCursorRow--;
+    //                 updateRowText();
+    //                 mCursorPos = max_index; // FIXME: Тут надо пересчитать количество символов на предыдущей строке и ставить это значение
+    //                 cursor.textIndex--;
+    //             }
+    //         } else if (key == GLFW_KEY_RIGHT) {
+    //             if (modifiers == GLFW_MOD_SHIFT) {
+    //                 if (mSelectionPos == -1)
+    //                     mSelectionPos = mCursorPos;
+    //             } else {
+    //                 mSelectionPos = -1;
+    //             }
+
+    //             // Тут срочно нужна проверка на максимальное значение в строке и перемещение курсора к последнему символу
+    //             if (mCursorPos < max_index) {
+    //                 mCursorPos++;
+    //                 cursor.textIndex++;
+    //             } else if (mCursorRow < max_rows-1) {
+    //                 mCursorRow++;
+    //                 mCursorPos = 0;
+    //                 cursor.textIndex++;
+    //             }
+    //         } else if (key == GLFW_KEY_UP) {
+    //             // Тут надо доработать селект строк
+    //             mSelectionPos = -1;
+    //             if (mCursorRow > 0) {
+    //                 mCursorRow--;
+    //                 updateRowText();
+    //                 if (mCursorPos > max_index) {
+    //                     mCursorPos = max_index;
+    //                 }
+    //             }
+    //         } else if (key == GLFW_KEY_DOWN) {
+    //             // Тут надо доработать селект строк
+    //             mSelectionPos = -1;
+    //             if (mCursorRow < max_rows-1) {
+    //                 mCursorRow++;
+    //                 updateRowText();
+    //                 if (mCursorPos > max_index) {
+    //                     mCursorPos = max_index;
+    //                 }
+    //             }
+    //         } else if (key == GLFW_KEY_HOME) {
+    //             if (modifiers == GLFW_MOD_SHIFT) {
+    //                 if (mSelectionPos == -1)
+    //                     mSelectionPos = mCursorPos;
+    //             } else {
+    //                 mSelectionPos = -1;
+    //             }
+
+    //             mCursorPos = 0;
+    //         } else if (key == GLFW_KEY_END) {
+    //             if (modifiers == GLFW_MOD_SHIFT) {
+    //                 if (mSelectionPos == -1)
+    //                     mSelectionPos = mCursorPos;
+    //             } else {
+    //                 mSelectionPos = -1;
+    //             }
+
+    //             mCursorPos = (int) mValueTemp.size();
+    //         } else if (key == GLFW_KEY_BACKSPACE) {
+    //             if (!deleteSelection()) {
+    //                 if (cursor.textIndex > 0) {
+    //                     mValueTemp.erase(mValueTemp.begin() + cursor.textIndex - 1);
+    //                     mCursorPos--;
+    //                     cursor.textIndex--;
+    //                 }
+    //             }
+    //         } else if (key == GLFW_KEY_DELETE) {
+    //             if (!deleteSelection()) {
+    //                 if (cursor.textIndex < (int) mValueTemp.length())
+    //                     mValueTemp.erase(mValueTemp.begin() + cursor.textIndex);
+    //             }
+    //         } else if (key == GLFW_KEY_ENTER) {
+    //             mValueTemp.insert(cursor.textIndex, "\n");
+    //             // mCursorPos++;
+    //             mCursorRow++;
+    //             cursor.textIndex++;
+    //             updateRowText();
+    //             mCursorPos = 0;
+    //             // if (!mCommitted)
+    //             //     focusEvent(false);
+    //         } else if (key == GLFW_KEY_A && modifiers == SYSTEM_COMMAND_MOD) {
+    //             mCursorPos = (int) mValueTemp.length();
+    //             mSelectionPos = 0;
+    //         } else if (key == GLFW_KEY_X && modifiers == SYSTEM_COMMAND_MOD) {
+    //             copySelection();
+    //             deleteSelection();
+    //         } else if (key == GLFW_KEY_C && modifiers == SYSTEM_COMMAND_MOD) {
+    //             copySelection();
+    //         } else if (key == GLFW_KEY_V && modifiers == SYSTEM_COMMAND_MOD) {
+    //             deleteSelection();
+    //             pasteFromClipboard();
+    //         }
+
+    //         mValidFormat =
+    //             (mValueTemp == "") || checkFormat(mValueTemp, mFormat);
+    //     }
+
+        // return true;
+    // }
+
+    // return false;
+// }
